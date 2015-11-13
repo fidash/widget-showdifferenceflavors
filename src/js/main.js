f2079181e811bd13df56d22a003adad74f9f8985
mgarcia <miguelglafuente@gmail.com>
2015-11-13 14:05:32 +0100

    Changed style and removed dependencies not needed

--- src/js/main.js
+++ src/js/main.js
@@ -1,94 +1,6 @@
-/*globals MashupPlatform, objectDiff */
+/*globals MashupPlatform*/
 window.onload = function () {
     "use strict";
-
-    var stringifyObjectKey = function stringifyObjectKey (key) {
-        return /^[a-z_$][a-z0-9_$]*$/i.test(key) ?
-            key :
-            JSON.stringify(key);
-    };
-
-    var escapeHTML = function escapeHTML (string) {
-        return string.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    };
-
-    var inspect = function inspect (obj) {
-        switch (typeof obj) {
-        case 'object':
-            var properties = [];
-            for (var key in obj) {
-                properties.push(stringifyObjectKey(escapeHTML(key)) + '<span>: </span>' + inspect(obj[key]));
-            }
-            return '<span>{</span>\n<div class="level">' + properties.join('<span>,</span>\n') + '\n</div><span>}</span>';
-
-        case 'string':
-            return JSON.stringify(escapeHTML(obj));
-
-        default:
-            return escapeHTML(obj.toString());
-        }
-    };
-
-    var showDifferences = function showDifferences(diff, maxleft, maxright) {
-        var propleft = [];
-        var propright = [];
-
-        for (var key in diff) {
-            var changed = diff[key].changed;
-            var nspacesleft = " "; // new Array(Math.max(1, maxleft - key.length + 1)).join(" ");
-            var nspacesright = " "; // new Array(Math.max(1, maxright - key.length + 1)).join(" ");
-            switch (changed) {
-            case 'equals':
-                propleft.push(stringifyObjectKey(escapeHTML(key)) + '<span>:' + nspacesleft + '</span>' + inspect(diff[key].value));
-                propright.push(stringifyObjectKey(escapeHTML(key)) + '<span>:' + nspacesright + '</span>' + inspect(diff[key].value));
-                break;
-
-            case 'removed':
-                propleft.push('<del>' + stringifyObjectKey(escapeHTML(key)) + '<span>:' + nspacesleft + '</span>' + inspect(diff[key].value) + '</del>');
-                propright.push('<span></span>');
-                break;
-
-            case 'added':
-                propleft.push('<span></span>');
-                propright.push('<ins>' + stringifyObjectKey(escapeHTML(key)) + '<span>:' + nspacesright + '</span>' + inspect(diff[key].value) + '</ins>');
-                break;
-
-            case 'primitive change':
-                var prefixleft = stringifyObjectKey(escapeHTML(key)) + '<span>:' + nspacesleft + '</span>';
-                var prefixright = stringifyObjectKey(escapeHTML(key)) + '<span>:' + nspacesright + '</span>';
-                propleft.push('<delwrap>' + prefixleft + '<del class="key">' + escapeHTML(inspect(diff[key].removed)) + '</del>' + '</delwrap>');
-                propright.push('<inswrap>' + prefixright + '<ins class="key">' + escapeHTML(inspect(diff[key].added)) + '</ins>' + '</inswrap>');
-                break;
-
-            case 'object change':
-                var res = showDifferences(diff[key].diff);
-                propleft.push(stringifyObjectKey(key) + '<span>: </span>' + res.left);
-                propright.push(stringifyObjectKey(key) + '<span>: </span>' + res.right);
-                break;
-            }
-        }
-
-        // var result = {
-        //     left: '<span>{</span>\n<div class="level">' + propleft.join('<span>,</span>\n') + '\n</div><span>}</span>',
-        //     right: '<span>{</span>\n<div class="level">' + propright.join('<span>,</span>\n') + '\n</div><span>}</span>'
-        // };
-
-        var result = {
-            left: '<div>' + propleft.join('\n') + '\n</div>',
-            right: '<div>' + propright.join('\n') + '\n</div>'
-        };
-
-        return result;
-    };
-
-    var findMaximum = function findMaximum(obj) {
-        var m = 0;
-        var keys = Object.keys(obj);
-        for (var k in keys) {
-            m = Math.max(keys[k].length, m);
-        }
-        return m;
-    };
 
     MashupPlatform.wiring.registerCallback("compare", function (data) {
         var jdata;
@@ -97,21 +9,64 @@ window.onload = function () {
         } catch (err) {
             throw new MashupPlatform.wiring.ValueTypeError();
         }
+
+        jdata.from = (jdata.from == null) ? jdata.left : jdata.from;
+        jdata.to = (jdata.to == null) ? jdata.left : jdata.to;
+
         if (jdata.from == null || jdata.to == null) {
             throw new MashupPlatform.wiring.ValueTypeError();
         }
 
-        var fromnamehtml = "";
-        var tonamehtml = "";
+        var leftcpu = jdata.from.vcpus || 0,
+            leftdisk = jdata.from.disk || 0,
+            leftram = jdata.from.ram || 0,
+            rightcpu = jdata.to.vcpus || 0,
+            rightdisk = jdata.to.disk || 0,
+            rightram = jdata.to.ram || 0;
 
-        if (jdata.fromname != null && jdata.toname != null) {
-            fromnamehtml = "<name>" + jdata.fromname + "</name>\n\n";
-            tonamehtml = "<name>" + jdata.toname + "</name>\n\n";
-        }
+        var publicdisk = document.getElementById("publicdisk"),
+            publiccpu = document.getElementById("publiccpu"),
+            publicram = document.getElementById("publicram"),
+            privatedisk = document.getElementById("privatedisk"),
+            privatecpu = document.getElementById("privatecpu"),
+            privateram = document.getElementById("privateram");
 
-        var diff = objectDiff.diffOwnProperties(jdata.from, jdata.to);
-        var results = showDifferences(diff, findMaximum(jdata.from), findMaximum(jdata.to));
-        document.getElementById("original").innerHTML = fromnamehtml + results.left;
-        document.getElementById("newone").innerHTML = tonamehtml + results.right;
+        var maxl = Math.max(String(leftdisk).length,
+                            String(leftcpu).length,
+                            String(leftram).length);
+
+
+        var diskdiff = leftdisk !== rightdisk;
+        var ramdiff = leftram !== rightram;
+        var cpudiff = leftcpu !== rightcpu;
+        var diskgreater = leftdisk > rightdisk;
+        var ramgreater = leftram > rightram;
+        var cpugreater = leftcpu > rightcpu;
+
+        publicdisk.innerHTML = '<div class="' + ((diskgreater) ? "green" : "") + '" style="float: right;">' + leftdisk + '</div>';
+        publicdisk.style.width = maxl + 'em';
+        publiccpu.innerHTML = '<div class="' + ((cpugreater) ? "green" : "") + '" style="float: right;">' + leftcpu + '</div>';
+        publiccpu.style.width = maxl + 'em';
+        publicram.innerHTML = '<div class="' + ((ramgreater) ? "green" : "") + '" style="float: right;">' + leftram + '</div>';
+        publicram.style.width = maxl + 'em';
+
+        privatedisk.innerHTML = '<div class="' + ((diskdiff && !diskgreater) ? "green" : "") + '" style="float: left;">' + rightdisk + '</div>';
+        privatecpu.innerHTML = '<div class="' + ((cpudiff && !cpugreater) ? "green" : "") + '"  style="float: left;">' + rightcpu + '</div>';
+        privateram.innerHTML = '<div class="' + ((ramdiff && !ramgreater) ? "green" : "") + '" style="float: left;">' + rightram + '</div>';
+
+        // var diskdiff = leftdisk !== rightdisk;
+        // var ramdiff = leftram !== rightram;
+        // var cpudiff = leftcpu !== rightcpu;
+
+        // publicdisk.innerHTML = '<div class="' + ((diskdiff) ? "red" : "") + '" style="float: right;">' + leftdisk + '</div>';
+        // publicdisk.style.width = maxl + 'em';
+        // publiccpu.innerHTML = '<div class="' + ((cpudiff) ? "red" : "") + '" style="float: right;">' + leftcpu + '</div>';
+        // publiccpu.style.width = maxl + 'em';
+        // publicram.innerHTML = '<div class="' + ((ramdiff) ? "red" : "") + '" style="float: right;">' + leftram + '</div>';
+        // publicram.style.width = maxl + 'em';
+
+        // privatedisk.innerHTML = '<div class="' + ((diskdiff) ? "green" : "") + '" style="float: left;">' + rightdisk + '</div>';
+        // privatecpu.innerHTML = '<div class="' + ((cpudiff) ? "green" : "") + '"  style="float: left;">' + rightcpu + '</div>';
+        // privateram.innerHTML = '<div class="' + ((ramdiff) ? "green" : "") + '" style="float: left;">' + rightram + '</div>';
     });
 };
